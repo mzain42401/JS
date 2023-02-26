df028c9c8bdf1c4e4e1368e64d0fc186c23483b5
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-02-26 22:19:15 +0500

    update

---
+++ app.js
@@ -0,0 +1,13 @@
+// premetive data type====================================
+// 1-number , 2-string , 3-boolean
+
+// increment & decrement type=============================
+// pre-inc/dec = ++num & --num
+// post-inc/dec = num++ & num--
+
+// prompt give string bydefault===========================
+// if we need number from prompt we use + before prompt : +prompt()
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,7 @@
+
+var num=5;
+num=num + 10;
+
+// alert(4 +  num / 3)
+// alert("9a" -num + num + "5"); 
+alert(num%5 )
