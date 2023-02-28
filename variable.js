fa65d328116eaa88ab7f119e230259b816a10d23
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-02-28 23:40:34 +0500

    update

--- variable.js
+++ variable.js
@@ -1,7 +1,12 @@
 
-var num=5;
-num=num + 10;
+// var num=5;
+// num=num + 10;
 
 // alert(4 +  num / 3)
 // alert("9a" -num + num + "5"); 
-alert(num%5 )
+// alert(num%5 )
+
+var a=10;
+var b=  +prompt("Enter Value");
+console.log(a+b);
+
