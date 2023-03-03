204a710dde4ef682fc3af7c717d84a8d67fec2a2
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-03 22:42:13 +0500

    table

---
+++ table.js
@@ -0,0 +1,7 @@
+let InputNumber1=+prompt("Which table you want ")
+
+for(let i=1;i<=10;i++){
+
+    let result=InputNumber1*i;
+    console.log(result);
+}
--- variable.js
+++ variable.js
@@ -6,7 +6,8 @@
 // alert("9a" -num + num + "5"); 
 // alert(num%5 )
 
-var a=10;
-var b=  +prompt("Enter Value");
-console.log(a+b);
+// var a=10;
+// var b=  +prompt("Enter Value");
+// console.log(a+b);
+
 
