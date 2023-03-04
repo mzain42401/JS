025d843f3c12918aec7f51df2d77cb8dd80452e8
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-04 18:07:48 +0500

    change

--- table.js
+++ table.js
@@ -1,7 +1,6 @@
 let InputNumber1=+prompt("Which table you want ")
-
 for(let i=1;i<=10;i++){
-
     let result=InputNumber1*i;
-    console.log(result);
+    document.write(InputNumber1 + " x " + i + " = " + result + `</br>`) 
+    
 }
