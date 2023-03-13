88dcdc6b4efc49312adba2954cd8c11f29fe159b
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-14 00:03:11 +0500

    practice

--- array/app.js
+++ array/app.js
@@ -1,6 +1,56 @@
+
+// var user = prompt("enter city name")
+var arry = [ "karachi", "lahore", "Islambad"]
  
- 
- var arry=["Madina","karachi", "lahore", "Islambad" ]
+//=================push=========================
+//  used for add any string in the last of array
 
 
-console.log(arry);
+// if (user === "jhelum") {
+//     arry.push(user)
+// }
+// arry.push("jdkj")
+
+
+//=================pop=========================
+//  used for remove any string in the last of array
+
+
+// arry.pop()
+
+
+
+//=================unshift=========================
+//  used for add any string in the start of array
+
+
+// arry.unshift("zain")
+
+
+
+//=================shift=========================
+//  used for remove any string in the start of array
+
+
+// arry.shift()
+
+
+
+//=================splice=========================
+//  used for remove/add any string in the  array
+
+
+// arry.splice(1,1,'zain')
+
+
+
+//=================slice=========================
+//  it return a new array from previous array
+
+
+
+// console.log(arry.slice(0,1));
+
+
+
+
---
+++ forLoop/app.js
@@ -0,0 +1,27 @@
+// var user = prompt("enter name")
+// var arry = ["zain", "saad", "ali"]
+// var a = true
+// for (var i = 0; i < arry.length; i++) {
+//     if (user === arry[i]) {
+//         console.log('found');
+// a=false
+//         break
+//     }
+//     console.log(i);
+
+
+// }
+// if(a){
+// console.log('zain');
+// }
+
+
+// ================loop table 
+
+// var user=prompt("number")
+// for(var i=1;i<=10;i++){
+    
+//     console.log(user + " x " + i + " = " + user*i);
+    
+    
+// }
