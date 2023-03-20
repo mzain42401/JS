94b97d0eb58d8ee39f449fa5114afba4b7ae99c8
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-20 22:03:30 +0500

    practice

--- nestedLoop/app.js
+++ nestedLoop/app.js
@@ -12,10 +12,15 @@
 
 
 var text ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, aperiam. Quidem atque dolore optio eaque? Minus World War II nemo sequi ullam. Aliquid optio perferendis, et odit nobis ratione maiores nisi sit deserunt.";
+var find=["World War II","ullam"]
 for (var i = 0; i < text.length; i++) {
-     if (text.slice(i, i + 12) === "World War II") {
-     text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+     for(var j=0;j<find.length;j++){
+          if (text.slice(i, i + 12) === find[j] ) {
+               text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+          }
+          console.log("z");
      }
      }
     
+    console.log(text);
     console.log(text);
