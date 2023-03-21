304ee629f7743876dca41e77a31c5c4008704cc7
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-21 23:18:08 +0500

    practice

---
+++ assignment/app.js
@@ -0,0 +1,45 @@
+//  ========================= Question no 3
+
+// for(var i=1;i<=10;i++){
+// console.log(i);
+// }
+
+
+// ========================== Question no 4
+
+// var tableNum=prompt("enter a number to show its multiplicatio table")
+// var lengthNum=+prompt("enter a length multiplication table")
+// for(var i=1;i<=lengthNum;i++){
+//     console.log(tableNum + " x " + i + " = " + tableNum*i);
+// }
+
+
+// ========================== Question no 5
+
+// var fruits=["apple","banana","mango","orange","strawberry"]
+// for(var i=0;i<fruits.length;i++){
+// console.log(fruits[i]);
+// }
+
+
+
+// ========================== Question no 6
+
+// a
+
+// for(var i=1;i<=15;i++){
+//     console.log(i);
+// }
+
+// b 
+
+// for(var i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// c 
+
+for(var i=0;i<=10;i++){
+    console.log(i);
+    // break
+}
--- nestedLoop/app.js
+++ nestedLoop/app.js
@@ -11,16 +11,27 @@
 // console.log(fullNames);
 
 
-var text ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, aperiam. Quidem atque dolore optio eaque? Minus World War II nemo sequi ullam. Aliquid optio perferendis, et odit nobis ratione maiores nisi sit deserunt.";
-var find=["World War II","ullam"]
-for (var i = 0; i < text.length; i++) {
-     for(var j=0;j<find.length;j++){
-          if (text.slice(i, i + 12) === find[j] ) {
-               text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
-          }
-          console.log("z");
-     }
-     }
+// var text ="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, aperiam. Quidem atque dolore optio eaque? Minus World War II nemo sequi ullam. Aliquid optio perferendis, et odit nobis ratione maiores nisi sit deserunt.";
+// var find=["World War II","ullam"]
+// for (var i = 0; i < text.length; i++) {
+//      for(var j=0;j<find.length;j++){
+//           if (text.slice(i, i + 12) === find[j] ) {
+//                text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//           }
+//           console.log("z");
+//      }
+//      }
     
-    console.log(text);
-    console.log(text);
+//     console.log(text);
+//     console.log(text);
+
+
+
+var city=["karachi","pakistan","islambad","jamshoro","sakkar"]
+var userCity=prompt("enter city name")
+for(var i=0;i<city.length;i++){
+if(userCity===city[i]){
+     console.log("zain");
+
+}
+}
