7b86849534b0c311c49d174421d2053e790e1b1d
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-26 23:35:13 +0500

    practice

---
+++ clock/app.js
@@ -0,0 +1,17 @@
+var hours=document.getElementById("hours")
+var mint=document.getElementById("mint")
+var sec=document.getElementById("sec")
+var amPm=document.getElementById("amPm")
+
+
+var date=new Date();
+
+var Hour=date.getHours();
+var mintue=date.getMinutes();
+var second=date.getSeconds();
+var day=date.getDay()
+console.log(day);
+
+
+
+
