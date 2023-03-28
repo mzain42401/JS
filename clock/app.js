59c9466d051421272dcc31af19c0aedda24c8403
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-03-28 16:09:39 +0500

    practice

--- clock/app.js
+++ clock/app.js
@@ -2,7 +2,9 @@ var hours = document.getElementById("hours")
 var mint = document.getElementById("mint")
 var sec = document.getElementById("sec")
 var amPm = document.getElementById("amPm")
-
+var date=document.getElementById("date")
+var day=document.getElementById("day")
+var month=document.getElementById("month")
 
 
 
@@ -11,12 +13,36 @@ setInterval(function(){
     hours.innerText=newDate.getHours();
     mint.innerText=newDate.getMinutes();
     sec.innerText=newDate.getSeconds();
-    if(hours.innerText>12){
-        hours.innerText-= 12
-        amPm.innerHTML="pm"
+    date.innerText=newDate.getDate();
+    
+
+    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    var newDay=newDate.getDay()
+    day.innerText=days[newDay]
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var newMonth=newDate.getMonth();
+month.innerText=months[newMonth]
+
+
+   
+    if(hours.innerText>11){
+        
+        amPm.innerHTML="PM"
         
     }
+    else{
+        amPm.innerHTML="AM"
+
+    }
+    
+    if(hours.innerText>12){
+        hours.innerText-= 12
+    }
+    if(hours.innerText==0){
+        hours.innerText=12
+     }
 },1000)
+
 
 
 
