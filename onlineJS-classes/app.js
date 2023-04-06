0f75c50475e94999f3dcc46037e6600c4be44be9
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-04-06 18:45:37 +0500

    practice

--- onlineJS-classes/app.js
+++ onlineJS-classes/app.js
@@ -1,20 +1,80 @@
-// ---------------------wellcome for children
+// Question No. 01
+
+// let firstNum=+prompt("enter first number");
+// let secondNum=+prompt("enter second number");
+
+// let largeNum=firstNum;
+// if(secondNum>largeNum){
+//     largeNum=secondNum;
+// }
+// console.log(largeNum);
 
 
-let userAge=+prompt("enter your age")
-if(userAge<=10){
-    console.log("wellcome");
-}
-else{
-    console.log("sorry your age is more than requirment");
-
-}
+// Question No. 02
 
 
-// --------------------Table
+// let num=+prompt("enter any number");
+// if(num>0){
+//     console.log("+ " + num);
+// }
+// else {
+//     console.log(+ num);
+
+// }
 
 
-let userNum=+prompt("which table you want")
-for(let i=1;i<=10;i++){
-    console.log(userNum + " X " + i + " = " + userNum*i);
-}
+
+
+
+//  Question No.03
+
+// let num1 = +prompt("enter first number");
+// let num2 = +prompt("enter second number");
+// let num3 = +prompt("enter third number");
+// let num4 = +prompt("enter fourth number");
+// let num5 = +prompt("enter fifth number");
+
+
+// let largeNumber = num1;
+
+// if (num2 > largeNumber) {
+//     largeNumber = num2;
+// }
+
+// if (num3 > largeNumber) {
+//     largeNumber = num3;
+// }
+
+// if (num4 > largeNumber) {
+//     largeNumber = num4;
+// }
+
+// if (num5 > largeNumber) {
+//     largeNumber = num5;
+// }
+
+// console.log("The larger number is " + largeNumber);
+
+
+
+
+// Question No. 04
+
+
+// for(let i=0;i<=15;i++){
+//     if(i%2===0){
+//         console.log(i +" is even number");
+//     }
+//     else{
+//         console.log(i +" is odd number");
+//     }
+// }
+
+
+
+// Question No. 05
+
+
+let userNum=+prompt("enter your obtained number");
+let percentage=userNum/100*100
+if(percentage )
