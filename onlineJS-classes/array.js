0985ed42720497e4b9f391b5fc4ef99313a44b33
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-04-08 23:55:59 +0500

    practice

--- onlineJS-classes/array.js
+++ onlineJS-classes/array.js
@@ -1,13 +1,19 @@
-let arry=[1,2,3,4,5,6,7,8,9,10]
-
+// let arry=[1,2,3,4,5,6,7,8,9,10]
 // arry.pop();
 // arry.push(11);
 // arry.shift();
 // arry.unshift(0);
+// let fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// fruits.splice(2, 2,"strawbery");
+// let updatedArray=fruits.slice(2,4);
+// let fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// let animal=["cat","dog","loin"]
+// let result=fruits.concat(animal)
 
-arry.splice(4,2,9999999,1078878892)
-// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
-// fruits.splice(2, 2);
+
+console.log(result);
 
 
-console.log(arry);
+
+
+
