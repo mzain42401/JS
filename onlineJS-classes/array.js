ca95d5fcc4164e28a853efa1ac7352c4656c030f
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-04-09 11:36:05 +0500

    practice

--- onlineJS-classes/array.js
+++ onlineJS-classes/array.js
@@ -3,15 +3,61 @@
 // arry.push(11);
 // arry.shift();
 // arry.unshift(0);
+
 // let fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 // fruits.splice(2, 2,"strawbery");
+
 // let updatedArray=fruits.slice(2,4);
+
 // let fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
 // let animal=["cat","dog","loin"]
 // let result=fruits.concat(animal)
 
+//  let animal=["cat","dog","loin",99]
+//  let b =animal.join("-")
 
-console.log(result);
+// let a=10;
+// let b="zain"
+// let c =["zain",33]
+// let check=Array.isArray(c)
+
+// let fruits = ["Banana", "Orange", "Apple","Orange", "Mango", "Kiwi"];
+// let index=fruits.indexOf("Orange")
+//  let index=fruits.lastIndexOf("Orange")
+
+// let fruits = ["Banana", "Orange", "Apple","Orange", "Mango", "Kiwi"];
+// let find=fruits.find(()=>{
+//     if(fruits.indexOf("Apple")===2){
+//         console.log("zain");
+//     }
+// })
+
+// let fruits = ["Banana", "Orange", "Apple","Orange", "Mango", "Kiwi"];
+// let New=fruits.toString()
+
+
+
+// let fruits = ["Banana", "Orange", "Apple","Orange", "Mango", "Kiwi"];
+// var i=0
+// while(i<fruits.length){
+//     console.log(fruits[i]);
+//     i++
+// }
+
+// var i=0
+// do{
+//      console.log(fruits[i]);
+//      i++
+// }while(i<fruits.length)
+
+// for(var i=0;i<fruits.length;i++){
+//     console.log(fruits[i]);
+
+// }
+
+
+
+
 
 
 
