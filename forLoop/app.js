3c25a509f6279ba041d3049d30d84179abd17bb9
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-04-30 23:59:47 +0500

    practice

--- forLoop/app.js
+++ forLoop/app.js
@@ -22,15 +22,34 @@
 // for(var i=1;i<=10;i++){
     
 //     console.log(user + " x " + i + " = " + user*i);
-    
-    
+     
+     
 // }
 
-let user=prompt();
-let userLength=user.length;
-for(let i=0; i<userLength; i++){
-if(user.charAt(i)==="@"){
-alert("zan")
-}
-}
+// let user=prompt();
+// let userLength=user.length;
+// for(let i=0; i<userLength; i++){
+// if(user.indexOf("@")===-1){
+// alert("use @ ")
+// break
+// }
+// }
 
+
+
+var user = prompt("enter name")
+var arry = ["zain", "saad", "ali"]
+var a = true
+for (var i = 0; i < arry.length; i++) {
+    if (user === arry[i]) {
+        console.log('found');
+a=false
+        break
+    }
+    console.log(i);
+
+
+}
+if(a){
+console.log('zain');
+}
