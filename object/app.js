6fcc265da6ea1dd8c595f8117b71089e97186c28
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-05-09 17:30:34 +0500

    practice

--- object/app.js
+++ object/app.js
@@ -1,28 +1,77 @@
-var student = {
-    name: "zain",
-    age: 19,
-    roll: 136,
-    className: "matric",
-    color: "white",
-     disctMon : [1, 2, 3, 4, 5],
-    price: 100,
-    anuualPrice:function disct(distPercent) {
-
-        var date = new Date()
-        var mon = date.getMonth()
-        for (var i = 0; i < this.disctMon.length; i++) {
-            if (mon === this.disctMon[i]) {
-                var newPrice = student.price * distPercent
-                break
-            }
-        }
-        return newPrice
-    
-    }
+function Student (name,roll,height,weigth) {
+this.name=name;
+this.roll=roll;
+this.height=height;
+this.weigth=weigth
 }
+var zain=new Student("zain",9022,5.7,"67kg")
+console.log(zain);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var obj={
+//     name:"ZAIN",
+//     roll:930,
+//     class:"matric"
+// }
+
+// console.log(obj);
+// delete obj.class
+// console.log(obj);
+// obj.roll=1222
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var student = {
+//     name: "zain",
+//     age: 19,
+//     roll: 136,
+//     className: "matric",
+//     color: "white",
+//      disctMon : [1, 2, 3, 4, 5],
+//     price: 100,
+//     anuualPrice:function disct(distPercent) {
+
+//         var date = new Date()
+//         var mon = date.getMonth()
+//         for (var i = 0; i < this.disctMon.length; i++) {
+//             if (mon === this.disctMon[i]) {
+//                 var newPrice = student.price * distPercent
+//                 break
+//             }
+//         }
+//         return newPrice
+    
+//     }
+// }
 // var key = "color"
 // console.log(student[key]);
 
-var price=student.anuualPrice(.9)
-console.log(student);
-console.log(price);
+// var price=student.anuualPrice(.9)
+// console.log(student);
+// console.log(price);
