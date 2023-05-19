5302769e6ffeee6d5acffd1e32c8ddda9d08f75b
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-05-19 16:15:26 +0500

    practice

---
+++ string/app.js
@@ -0,0 +1,15 @@
+
+
+// var num="88.990000077"
+// var str1="zain"
+// var str2="ali"
+// var done=str1 + str2
+// console.log(Number(num));
+
+// var a=89
+// var b=7
+// console.log(a%b);
+
+// var a="zain ali 42401"
+// var b=5+4*2
+console.log(5**2);
